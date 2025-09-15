5c55173aff9ca8612ac0bdcfe5fc88fc20c01af5
Kobayashi100 <kobakobainfo@gmail.com>
2025-09-15 04:05:12 +0000

    router install

---
+++ my-vue-app-2/src/pages/Login.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="loginTitle">ログイン</div>
+  <div class="loginComment">ユーザーを選択してください</div>
+  <button @click="goIndex('ひなた')">ひなた</button>
+  <button @click="goIndex('まなみ')">まなみ</button>
+</template>
